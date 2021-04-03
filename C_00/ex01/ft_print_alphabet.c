22f95ca36594d228b758644bce1355686c290a8c
Inês A. Sousa <isousa@student.42lisboa.com>
2021-04-03 20:07:41 +0100

    C_00 PASSED 80% ( Commented Version )

--- C_00/ex01/ft_print_alphabet.c
+++ C_00/ex01/ft_print_alphabet.c
@@ -23,3 +23,26 @@ void	ft_print_alphabet(void)
 		i++;
 	}
 }
+
+/*	READ THIS IF YOU DIND'T UNDERSTAND THE CODE:
+**
+**   *Make sure you understand ft_putchar so you can understand this one.
+**
+**   In ft_print_alphabet() function, you don't receive anything by argument, so 
+**	you need to create a variable of type char that I called 'i'. You do this
+**	declaring a variable 'char i;'
+**   Then you say 'i = 'a';' so you save the value 'a' inside of the variable 'i'.
+**	Note that saying 'i = 97;' would be exactly the same. Check the ASCII table.
+**
+**   The while() is a loop, it means it will repeat a command while 'something' that
+**	is inside the brackets is true. In this case it will repeat what is inside the
+**	the curly brackets '{ ... }' while the variable 'i' it's minor or equal to the value 'z'.
+**   If you notice, everytime it enters the while() it will write the value of 'i' (has we saw on ft_putchar()),
+**	and the, the value of 'i' will be incremented. So 'i++;' is the same as saying 'i = i + 1;'.
+**   The first time the variable 'i' enters the while, 'i' has the value of 'a' or 97 and we can see that 'a' is minor
+**	that 'z', the same way 97 is minor than 122 (122 is the value of 'z' in ASCII table), so the while's condition
+**	is true. Being true, it will print an 'a' using write() function and then increment (so, 'a' + 1 = 'b', or 
+**	97 + 1 = 98, and 98 is the value of 'b'.
+**   The loop will continue until the value of 'i' is bigger than 'z' (or 122). When it happens, all lowercase alphabet
+**	has been printed in the terminal.
+*/
